88d5319d36c97c95da396b427a19af277b005d91
Mattieu <mattieu_racine@hotmail.fr>
2016-01-15 16:02:54 +0100

    CreatePublicKey

---
+++ src/Functions.java
@@ -0,0 +1,29 @@
+import java.math.BigInteger;
+import java.util.Random;
+
+public class Functions {
+
+    public static final int BIT_LENGTH_PUBLIC = 10;
+
+    public static BigInteger[] createPublicKey() {
+
+        BigInteger p = BigInteger.probablePrime(BIT_LENGTH_PUBLIC, new Random());
+        BigInteger q;
+
+        do {
+            q = BigInteger.probablePrime(BIT_LENGTH_PUBLIC, new Random());
+        } while (p == q);
+
+        BigInteger n = p.multiply(q);
+        BigInteger m = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
+
+        BigInteger e;
+        do {
+            e = BigInteger.probablePrime(BIT_LENGTH_PUBLIC, new Random());
+        } while (!m.gcd(e).equals(BigInteger.ONE));
+
+
+        BigInteger[] resultat = {n, e};
+        return resultat;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 /**
  * Created by etudiant on 15/01/16.
  */
-public class main {
+public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        Functions.createPublicKey();
     }
 
 }
