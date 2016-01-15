438fde0a4496bd2663e516fe1bf0b05eb50729a0
Guillaume <grandjean.guillaume1@gmail.com>
2016-01-15 18:05:28 +0100

    PrivateKey

--- src/Functions/Functions.java
+++ src/Functions/Functions.java
@@ -28,4 +28,40 @@ public class Functions {
         BigInteger[] resultat = {n, e, m};
         return resultat;
     }
+
+    /*$public static BigInteger pgcd(BigInteger p, BigInteger q) {
+        if (q == 0) return p;
+        else return pgcd(q, p.divide(q));
+    }*/
+
+    public static BigInteger[] createPrivateKey(BigInteger e, BigInteger m, BigInteger n) {
+
+        BigInteger e0 = e;
+        BigInteger m0 = m;
+        BigInteger p = BigInteger.valueOf(1);
+        BigInteger q = BigInteger.valueOf(0);
+        BigInteger r = BigInteger.valueOf(0);
+        BigInteger s = BigInteger.valueOf(1);
+
+
+        while (!m.equals(BigInteger.ZERO)) {
+            BigInteger c = e.remainder(m);
+            BigInteger quotient = e.divide(m);
+            e = m;
+            m = c;
+            BigInteger nouveau_r = p.subtract(quotient.multiply(r));
+            BigInteger nouveau_s = q.subtract(quotient.multiply(s));
+            p = r;
+            q = s;
+            r = nouveau_r;
+            s = nouveau_s;
+
+        }
+        BigInteger u = m0.add(p);
+
+        BigInteger[] privateKey = {n,u};
+
+        return privateKey;
+
+    }
 }
---
+++ src/functions.java
@@ -0,0 +1,11 @@
+import java.math.BigInteger;
+
+/**
+ * Created by etudiant on 15/01/16.
+ */
+public class functions {
+
+
+
+
+}
